2af1cd77ab02c0191a10116a3edcf34a2978a542
Bjorn Hansen <bjornh@microsoft.com>
2014-10-15 12:48:41 -0700

    initial setup of skeloton project using Quixote, jasemine, and karma

---
+++ js/test.js
@@ -0,0 +1,14 @@
+//require("../node_modules/quixote/dist/quixote.js")
+quixote = require("./quixote.js")
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        var frame = quixote.createFrame(600, 400, {
+                src: "/base/html/mypage.html"
+            },
+            function(frame) {
+                var position = frame.getElement("[foo]").getRawPosition();
+                expect(position.left).toBe(0);
+            });
+    });
+});
