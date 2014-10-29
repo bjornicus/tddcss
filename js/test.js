e69eb144113b632a7bbf8aa9d7d543b3f437bb6f
Bjorn Hansen <bjornh@microsoft.com>
2014-10-29 15:48:06 -0700

    update test to be based on quixote sample from the documentation adjust mypage.html and mypage.css to match

--- js/test.js
+++ js/test.js
@@ -2,16 +2,56 @@
 quixote = require("../node_modules/quixote/dist/quixote.js")
 var expect = require('../node_modules/expect.js');
 
-describe("A suite", function() {
-    it("contains spec with an expectation", function(done) {
-        var frame = quixote.createFrame(600, 400, {
-                src: "/base/html/mypage.html"
-            },
-            function(err, frame) {
-                var element = frame.getElement("[foo]");
-                var position = element.getRawPosition();
-                expect(position.top).to.be(25);
-                done();
-            });
+describe("Example", function() {
+
+  var frame;        // Quixote test frame
+  var sectionOne;         // the sectionOne element on the page
+  var sectionTwo;         // the sectionTwo element
+
+  // Create the test frame once for all your tests.
+  // Here we load example.html. You can also create elements programmatically.
+  before(function(done) {
+    var options = { src: "/base/html/mypage.html" };
+    frame = quixote.createFrame(options, done);
+  });
+
+  // Destroy the test frame after your tests are done.
+  after(function() {
+    frame.remove();
+  });
+
+  // Reset the test frame before (or after) each test. This is
+  // faster than re-creating the frame for every test.
+  beforeEach(function() {
+    frame.reset();
+
+    // Get the elements we want to test
+    sectionOne = frame.get("#foo");       // you can use any CSS selector
+    sectionTwo = frame.get("[data-section~='two']");
+  });
+
+  // Here's our test.
+  it("positions sectionTwo below sectionOne", function() {
+    // The 'assert()' method checks multiple properties at once.
+    // You can also use 'diff()' with your favorite assertion library.
+    sectionTwo.assert({
+      // We can check a hard-coded value
+      left: 15,    // sectionTwo is 15px from left side of page
+
+      // Or, better yet, check the relationship between elements
+      top: sectionOne.bottom.plus(10)   // sectionTwo is 10px below logo
     });
+  });
+
+  it("uses big font for sectionTwo", function() {
+    // Sometimes, 'assert()' doesn't support what you need.
+    // But you can check any CSS style you want by using 'getRawStyle()'.
+
+    // Get the font size actually displayed in the browser
+    var fontSize = sectionTwo.getRawStyle("font-size");  
+
+    // Use your preferred assertion library (such as Chai) to make assertions.
+    expect(fontSize).to.be("16px");
+  });
+
 });
